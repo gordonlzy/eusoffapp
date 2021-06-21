33c7192fd035819c5ab4c2dc15f570807f993f10
gordonlzy <gordonlzy123@gmail.com>
2021-06-21 15:41:53 +0800

    try cookie (testing)

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // set view engine
 app.set("view engine", "ejs");
@@ -20,3 +22,15 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true })
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+    res.cookie("newUser", false);
+    res.send("You got the cookies");
+})
+
+app.get('/read  -cookies', (req, res) => {
+    const cookies = res.cookies;
+    console.log(cookies);
+    res.json(cookies);
+})
