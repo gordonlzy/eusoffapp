808bcf158e198d3c1a35d933a224580b39f8b24a
gordonlzy <gordonlzy123@gmail.com>
2021-06-22 15:36:12 +0800

    add name and room for signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,21 +4,39 @@ const jwt = require('jsonwebtoken');
 // handle error
 const handleErrors = (err) => {
     console.log(err.message, err.code);
-    let errors = { email: "", password: "" };
+    let errors = { name: "", room: "", email: "", password: "" };
 
-    // incorrect email
+    // incorrect email (login)
     if (err.message === "incorrect email") {
         errors.email = "that email is not registered";
     }
 
-    // incorrect password
+    // incorrect password (login)
     if (err.message === "incorrect password") {
         errors.password = "that password is incorrect";
     }
 
     // duplicate error code
     if (err.code === 11000) {
-        errors.email = "that email is already registered";
+        const key = Object.keys(err.keyValue)[0];
+        const val = Object.values(err.keyValue)[0];
+        console.log(key, val);
+        console.log(key == "room", key == "email");
+        switch (key) {
+            case "name":
+                errors.name = "the name associated with this account is already registered";
+                break;
+            case "room":
+                errors.room = "the room associated with this account is already registered";
+                break;    
+            case "email":
+                errors.email = "the email associated with this account is already registered";
+                break;
+            default:
+                errors.name = "the name associated with this account is already registered";
+                errors.room = "the room associated with this account is already registered";
+                errors.email = "the email associated with this account is already registered";
+        }
         return errors;
     }
 
@@ -49,10 +67,10 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
-    const { email, password } = req.body;
+    const { name, room, email, password } = req.body;
     
     try {
-        const user = await User.create({ email, password });
+        const user = await User.create({ name, room, email, password });
         const token = createToken(user._id);
         res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(201).json({ user: user._id });
