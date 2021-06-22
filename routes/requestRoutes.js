f39250536e46bb7436c39e1c9ca9c88a8eda6c50
gordonlzy <gordonlzy123@gmail.com>
2021-06-22 15:38:24 +0800

    manage route for request

---
+++ routes/requestRoutes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const requestController = require('../controllers/requestController');
+
+const router = Router();
+
+router.get('/requests', requestController.request_index);
+router.get('/create', requestController.request_create_get);
+router.post('/requests', requestController.request_create_post);
+router.post('/:id', requestController.request_details);
+router.delete('/:id', requestController.request_delete);
+
+module.exports = router;
