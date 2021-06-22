59239edb087cd2e462de255feb896951337420c0
gordonlzy <gordonlzy123@gmail.com>
2021-06-22 15:37:46 +0800

    add controller for request

---
+++ controllers/requestController.js
@@ -0,0 +1,43 @@
+const Request = require('../models/Request');
+
+module.exports.request_index = (req, res) => {
+    Request.find().sort({ createdAt: -1 })
+        .then(result => {
+            res.render('favours/requests', { requests: result, title: "Eusoff Favours" });
+        })
+        .catch((err) => console.log(err))
+}
+
+module.exports.request_create_get = (req, res) => {
+    res.render('favours/create', { title: "Create a new request" });
+}
+
+module.exports.request_create_post = (req, res) => {
+    const request = new Request(req.body);
+    request.save()
+        .then(result => {
+            res.redirect("/requests");
+        })
+        .catch(err => console.log(err));
+}
+
+module.exports.request_details = (req, res) => {
+    const id = req.params.id;
+    Request.findById(id)
+        .then(result => {
+            res.render('favours/details', { request: result, title: "Request details" });
+        })
+        .catch(err => {
+            console.log(err);
+            res.render("404", { title: "Request not found" });
+        })
+}
+
+module.exports.request_delete = (req, res) => {
+    const id = req.param.id;
+    Request.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: "favours/requests" });
+        })
+        .catch(err => console.log(err));
+}
