f22428cfd229d680a7c2211fd29375c7d20b6dca
gordonlzy <gordonlzy123@gmail.com>
2021-07-03 02:01:57 +0800

    clean up route

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -3,10 +3,11 @@ const requestController = require('../controllers/requestController');
 
 const router = Router();
 
-router.get('/requests', requestController.request_index);
+router.get('', requestController.request_index);
 router.get('/create', requestController.request_create_get);
-router.post('/requests', requestController.request_create_post);
+router.post('', requestController.request_create_post);
 router.get('/:id', requestController.request_details);
 router.delete('/:id', requestController.request_delete);
+// router.post('/:id', requestController.request_take_post);
 
 module.exports = router;
