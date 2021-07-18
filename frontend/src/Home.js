f11e796162fcb8a806a19bcaf4068b776e905daf
gordonlzy <gordonlzy123@gmail.com>
2021-07-19 01:48:01 +0800

    update component for home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,21 @@
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <header>
+            <div class="flex-gap">
+            <div>
+                <h4>Meal Bot</h4>
+                <a href="/meal" class="btn">Check Menu</a>
+            </div>
+            <div>
+                <h4>Laundry Bot</h4>
+                <a href="/laundry" class="btn">Check Laundry</a>
+            </div>
+            <div>
+                <h4>Eusoff Favours</h4>
+                <a href="/favours" class="btn">Check Favours</a>
+            </div>
+            </div>
+        </header>
     );
 }
  
