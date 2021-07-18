1a82520b65bb85c57393cba8109ad43de56e3949
gordonlzy <gordonlzy123@gmail.com>
2021-07-19 01:47:50 +0800

    add component for navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -3,10 +3,24 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="links">
-                <Link to="/">Eusoff App</Link>
-                <Link to="/login">LOG IN</Link>
-                <Link to="/signup">SIGN UP</Link>
+            <h1><Link to="/">Eusoff App</Link></h1>
+            <div>
+                {/* { user && 
+                    <ul>
+                        <li><Link to="/profile/<%= user._id %>">Welcome, { user.name } ({ user.room })</Link></li>
+                        <li><Link to="/logout">Log out</Link></li>
+                    </ul>
+                }
+                { !user &&
+                    <ul>
+                        <li><Link to="/login">Log in</Link></li>
+                        <li><Link to="/signup" className="btn">Sign up</Link></li>
+                    </ul>
+                } */}
+                <ul>
+                    <li><Link to="/login">Log in</Link></li>
+                    <li><Link to="/signup" className="btn">Sign up</Link></li>
+                </ul>
             </div>
         </nav>
     );
