410479d70c843cafda54f0c04e93af3eaa030511
gordonlzy <gordonlzy123@gmail.com>
2021-07-19 01:46:37 +0800

    add links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Favours from './Favours';
 import Home from "./Home";
+import Laundry from './Laundry';
 import Login from './Login';
+import Meal from './Meal';
 import Navbar from "./Navbar";
 import NotFound from './NotFound';
 import SignUp from './SignUp';
@@ -20,6 +23,15 @@ function App() {
             </Route>
             <Route path="/signup">
               <SignUp />
+            </Route>
+            <Route path="/favours">
+              <Favours />
+            </Route>
+            <Route path="/meal">
+              <Meal />
+            </Route>
+            <Route path="/laundry">
+              <Laundry />
             </Route>
             <Route path="*">
               <NotFound />
