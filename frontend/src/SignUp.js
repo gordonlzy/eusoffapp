d35b9a84a5945eaa91ecfc5a89870155cb6df745
gordonlzy <gordonlzy123@gmail.com>
2021-07-19 01:47:44 +0800

    add component for signUp

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
     return (
-        <div>Sign Up</div>
+        <form>
+            <h2>Sign up</h2>
+            <label for="name">Name</label>
+            <input type="text" name="name" required />
+            <div class="name error"></div>
+            <label for="room">Room</label>
+            <input type="text" name="room" required />
+            <div class="room error"></div>
+            <label for="email">Email</label>
+            <input type="text" name="email" required />
+            <div class="email error"></div>
+            <label for="password">Password</label>
+            <input type="password" name="password" required />
+            <div class="password error"></div>
+            <div class="alr-signup">Already have an account? <Link to="/login">Log in</Link></div>
+            <input type="hidden" name="credit" value= "99" />
+            <button>Sign up</button>
+        </form>
     );
 }
  
