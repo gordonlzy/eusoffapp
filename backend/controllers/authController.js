903db77ca080bd8a2df5b974cd56dc9933cf4bcd
gordonlzy <gordonlzy123@gmail.com>
2021-07-24 13:52:45 +0800

    return jwt token instead of id

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -90,7 +90,8 @@ module.exports.login_post = async (req, res) => {
         const user = await User.login(email, password);
         const token = createToken(user._id);
         res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(200).json({ user: user._id });
+        // res.status(200).json({ user: user._id });
+        res.status(200).json({ token });
     }
     catch (err) {
         const errors = handleErrors(err);
