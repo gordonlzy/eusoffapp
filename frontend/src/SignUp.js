44d5532728ba45fc2f9474a2ae03828b3cc7415d
gordonlzy <gordonlzy123@gmail.com>
2021-08-03 19:57:52 +0800

    add sign up

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,24 +1,86 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
 
-const SignUp = () => {
+const SignUp = ({ stateChanger }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [roomError, setRoomError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [credit, setCredit] = useState(99);
+    const history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("in");
+
+        try {
+            const res = await fetch('http://localhost:8080/signup', {
+                method: 'POST',
+                body:  JSON.stringify({ name, room, email, password, credit }),
+                headers: { "Content-Type": "application/json" },
+                withCredentials: true,
+                credentials: 'include'
+            });
+            const data = await res.json();
+            if (data.errors) {
+                console.log("error");
+                setNameError(data.errors.name);
+                setRoomError(data.errors.room);
+                setEmailError(data.errors.email);
+                setPasswordError(data.errors.password);
+            }
+            if (data.token) {
+                stateChanger(true);
+                console.log("all good", data.token);
+                history.push("/");
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <form>
-            <h2>Sign up</h2>
-            <label for="name">Name</label>
-            <input type="text" name="name" required />
-            <div className="name error"></div>
-            <label for="room">Room</label>
-            <input type="text" name="room" required />
-            <div className="room error"></div>
-            <label for="email">Email</label>
-            <input type="text" name="email" required />
-            <div className="email error"></div>
-            <label for="password">Password</label>
-            <input type="password" name="password" required />
-            <div className="password error"></div>
+        <form onSubmit={handleSubmit}>
+            <h2>Sign Up</h2>
+            <label>Name</label>
+            <input 
+                required
+                type="text" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}  
+            />
+            <div className="name error">{ nameError }</div>
+            <label>Room</label>
+            <input 
+                required
+                type="text" 
+                value={room}
+                onChange={(e) => setRoom(e.target.value)}  
+            />
+            <div className="room error">{ roomError }</div>
+            <label>Email</label>
+            <input 
+                required
+                type="text" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}  
+            />
+            <div className="email error">{ emailError }</div>
+            <label>Password</label>
+            <input 
+                required
+                type="password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
+            />
+            <div className="password error">{ passwordError }</div>
             <div className="alr-signup">Already have an account? <Link to="/login">Log in</Link></div>
-            <input type="hidden" name="credit" value= "99" />
-            <button>Sign up</button>
+            <button>Sign Up</button>
         </form>
     );
 }
