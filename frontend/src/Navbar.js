dfd0ccec936c7c81488f2bdc7d9da85a8a5e8cdc
gordonlzy <gordonlzy123@gmail.com>
2021-08-03 19:59:52 +0800

    handle logout

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,26 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ user, login, stateChanger }) => {
+    const history = useHistory();
+    const handleLogout = async (e) => {
+        try {
+            const res = await fetch('http://localhost:8080/logout', {
+                method: 'GET',
+                withCredentials: true,
+                credentials: 'include'
+            });
+            const data = await res.json();
+            if (data.redirect) {
+                setIsUser(false);
+                stateChanger(false);
+                history.push("/");
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    const [isUser, setIsUser] = useState(user);
+
     return (
         <nav className="navbar">
             <h1><Link to="/">Eusoff App</Link></h1>
             <div>
-                {/* { user && 
+                { (login || isUser) &&
                     <ul>
-                        <li><Link to="/profile/<%= user._id %>">Welcome, { user.name } ({ user.room })</Link></li>
-                        <li><Link to="/logout">Log out</Link></li>
+                        <li><Link to={`/profile/${user._id}`}>Welcome, { user.name } ({ user.room })</Link></li>
+                        <li><a onClick={handleLogout}>Log out</a></li>
                     </ul>
                 }
-                { !user &&
+                { (!isUser && !login) &&
                     <ul>
                         <li><Link to="/login">Log in</Link></li>
                         <li><Link to="/signup" className="btn">Sign up</Link></li>
                     </ul>
-                } */}
-                <ul>
-                    <li><Link to="/login">Log in</Link></li>
-                    <li><Link to="/signup" className="btn">Sign up</Link></li>
-                </ul>
+                }
             </div>
         </nav>
     );
