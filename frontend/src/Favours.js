6e62eefdbe968fdad21445ceaa05057ef67b86b3
gordonlzy <gordonlzy123@gmail.com>
2021-08-03 19:58:16 +0800

    add user as props and remove axios

--- frontend/src/Favours.js
+++ frontend/src/Favours.js
@@ -1,14 +1,17 @@
 import RequestList from "./RequestList";
 import useFetch from "./useFetch";
-// import axios from 'axios';
+import { Link } from "react-router-dom";
+import React from "react";
 
-const Favours = () => {
+const Favours = ({ user }) => {
     const { data, isPending, error } = useFetch('http://localhost:8080/favours');
     return (
         <div className="favours">
             { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
-            { data && <RequestList requests={data.requests} /> }
+            { data && <RequestList requests={data.requests} user={user}/> }
+            <br />
+            <Link to="favours/create" className="create-btn">Create</Link>
         </div>
     );
 }
