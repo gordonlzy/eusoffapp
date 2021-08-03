45d5da5ba4a0cdf160b643c91a1553c8fb4d3c66
gordonlzy <gordonlzy123@gmail.com>
2021-08-03 19:57:25 +0800

    add inline editing

--- frontend/src/Details.js
+++ frontend/src/Details.js
@@ -4,29 +4,26 @@ import useFetch from "./useFetch";
 
 const Details = ({ user }) => {
     const { id } = useParams();
-    // const { data } = useFetch('http://localhost:8080/favours/' + id);
     const history = useHistory();
 
-    // const [editCategory, setEditCategory] = useState(`${ data.request.category }`);
-    // const [editCredit, setEditCredit] = useState(`${ data.request.credit }`);
-    // const [editRemark, setEditRemark] = useState(`${ data.request.remark }`);
     const [editCategory, setEditCategory] = useState("");
     const [editCredit, setEditCredit] = useState("");
     const [editRemark, setEditRemark] = useState("");
 
-    const { data, isPending } = useFetch('http://localhost:8080/favours/' + id)
-    // if (data) {
-    //         setEditCategory(data.request.category);
-    //         setEditCredit(data.request.credit);
-    //         setEditRemark(data.request.remark);
-    // }
-        // .then(data => {
-        //     setEditCategory(data.request.category);
-        //     setEditCredit(data.request.credit);
-        //     setEditRemark(data.request.remark);
-        // })
-    // const { myPromise } = useFetch('http://localhost:8080/favours/' + id)
-    //     .then(context => console.log("gay"));
+    const [backupCategory, setBackupCategory] = useState("");
+    const [backupCredit, setBackupCredit] = useState("");
+    const [backupRemark, setBackupRemark] = useState("");
+
+    const setData = (data) => {
+        setEditCategory(data.request.category);
+        setEditCredit(data.request.credit);
+        setEditRemark(data.request.remark);
+
+        setBackupCategory(data.request.category);
+        setBackupCredit(data.request.credit);
+        setBackupRemark(data.request.remark);
+    }
+    const { data } = useFetch('http://localhost:8080/favours/' + id, setData)
 
     const handleEdit = (e) => {
         console.log(id);
@@ -63,6 +60,8 @@ const Details = ({ user }) => {
         const endpoint = `http://localhost:8080/favours/${trashcan.dataset.doc}`;
         fetch(endpoint, {
             method: 'DELETE',
+            withCredentials: true,
+            credentials: 'include'
         })
         .then(response => response.json())
         .then(data => window.location.href = data.redirect)
@@ -82,15 +81,96 @@ const Details = ({ user }) => {
         .catch(err => console.log(err));
     }
 
+    const saveEdit = (e) => {
+        e.preventDefault();
+
+        const editRequest = { category:editCategory, credit:editCredit, remark:editRemark };
+
+        fetch("http://localhost:8080/favours/" + id, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(editRequest),
+            withCredentials: true,
+            credentials: 'include'
+        }).then(() => {
+            setBackupCategory(editCategory);
+            setBackupCredit(editCredit);
+            setBackupRemark(editRemark);
+            console.log("edited successfully");
+        })
+    }
+
+    const cancelEdit = () => {
+        setEditCategory(backupCategory);
+        setEditCredit(backupCredit);
+        setEditRemark(backupRemark);
+    }
+
     return (
         <div >
             { data && (
                 <div className="details content">
-                    <h1> { data.request.category } - { data.request.credit }  credit(s)</h1>
+                    <div className="edit-content">
+                        { user._id === data.request.owner && (
+                            <input
+                                value={editCategory}
+                                className="editable"
+                                onChange={(e) => setEditCategory(e.target.value)}
+                            />
+                        )}
+                        { user._id !== data.request.owner && (
+                            <p>Category: {editCategory}</p>
+                        )}
+                        {
+                            editCategory !== backupCategory && 
+                            <div className="inner-edit-content">
+                                <p className="save-changes" onClick={saveEdit}>Save</p>
+                                <p className="cancel-changes" onClick={cancelEdit}>Cancel</p>
+                            </div>
+                        }
+                    </div>
+                    <div className="edit-content">
+                        { user._id === data.request.owner && (
+                            <input
+                                value={editCredit}
+                                className="editable"
+                                onChange={(e) => setEditCredit(e.target.value)}
+                            />
+                        )}
+                        { user._id !== data.request.owner && (
+                            <p>Credit: {editCredit}</p>
+                        )}
+                        {
+                            editCredit != backupCredit && 
+                            <div className="inner-edit-content">
+                                <p className="save-changes" onClick={saveEdit}>Save</p>
+                                <p className="cancel-changes" onClick={cancelEdit}>Cancel</p>
+                            </div>
+                        }
+                    </div>
+                    <div className="edit-category">
+                        { user._id === data.request.owner && (
+                            <input
+                                value={editRemark}
+                                className="editable"
+                                onChange={(e) => setEditRemark(e.target.value)}
+                            />
+                        )}
+                        { user._id !== data.request.owner && (
+                            <p>Remark: {editRemark}</p>
+                        )}
+                        {
+                            editRemark !== backupRemark && 
+                            <div className="inner-edit-content">
+                                <p className="save-changes" onClick={saveEdit}>Save</p>
+                                <p className="cancel-changes" onClick={cancelEdit}>Cancel</p>
+                            </div>
+                        }
+                    </div>
+                    
                     <div className="content">
-                        <p>{ data.request.remark }</p>
                         <p>made by { data.request.ownerName }</p>
-                        { !data.request.takenBy && <p>Your request is not taken yet</p> }
+                        { !data.request.takenBy && <p>Request is not taken yet</p> }
                         { data.request.takenBy && 
                             <p>taken by 
                                 <a href={`/favours/profile/${ data.request.takenBy }`} 
@@ -99,52 +179,18 @@ const Details = ({ user }) => {
                             </p>
                             // { user.id == request.owner && request.status === "Taken"  &&
                             //     <div>
-                            //         <a className="remove-btn" id={`r_${request._id}`} onclick="remove(`r_${request._id}`)" data-request={`${request._id}`} data-user={`${user._id}`}>Remove Taker</a>
-                            //         <a className="remove-btn" id={`c_${request._id}`} onclick="completeRequest('c_${request._id}')" data-request="<%= request._id %>" data-user="<%= user._id %>" data-takenBy="<%= request.takenBy %>">Complete Request</a>
+                            //         <a className="remove-btn" id={`r_${request._id}`} onClick={remove(`r_${request._id}`)} data-request={`${request._id}`} data-user={`${user._id}`}>Remove Taker</a>
+                            //         <a className="remove-btn" id={`c_${request._id}`} onClick={completeRequest(`c_${request._id}`)} data-request={`${request._id}`} data-user={`${user._id}`} data-takenBy={`${request.takenBy}`}>Complete Request</a>
                             //     </div>
                             // }
                         }
                     </div>
                     { user._id == data.request.owner && (
                         <div>
-                            <a className="edit" data-doc={`${ data.request._id }`}>
-                                EDIT
-                            </a>
-                            <a className="delete" data-doc={`${ data.request._id }`} onClick="deleteRequest()">
+                            <a className="delete-btn" data-doc={`${ data.request._id }`} onClick={deleteRequest}>
                                 DELETE
                             </a>
                         </div>
-                    )}
-                    { user._id === data.request.owner && (
-                        <form onSubmit={handleEdit}>
-                            <h2>Edit request</h2>
-                            <label>Request category:</label>
-                            <input 
-                                required 
-                                type="text" 
-                                id="category" 
-                                value={editCategory} 
-                                onChange={(e) => setEditCategory(e.target.value)} 
-                            />
-                            <div className="category error"></div>
-                            <label>Request credit:</label>
-                            <input 
-                                required 
-                                type="text" 
-                                id="credit" 
-                                value={editCredit} 
-                                onChange={(e) => setEditCredit(e.target.value)} 
-                            />
-                            <div className="credit error"></div>
-                            <label>Request remark:</label>
-                            <textarea 
-                                required 
-                                id="remark" 
-                                onChange={(e) => setEditRemark(e.target.value)} 
-                            >{ data.request.remark }</textarea>
-                            <div className="remark error"></div>
-                            <button>Edit</button>
-                        </form>
                     )}
                 </div>
             )} 
